9d93cc0385f30f5b9faea51dc2d41888545db060
ahmad_alshamary <sotaalshamary@gmail.com>
2026-06-30 00:50:03 +0300

    solved anagram

---
+++ Valid-Anagram(easy).js
@@ -0,0 +1,40 @@
+// 242. Valid Anagram
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "rat", t = "car"
+
+// Output: false
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+// Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+// ---------------------------------------------------------------------------------------------------------
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  sortedS = s.split("").sort().join("");
+  sortedT = t.split("").sort().join("");
+
+  if (sortedS == sortedT) return true;
+  else return false;
+};
